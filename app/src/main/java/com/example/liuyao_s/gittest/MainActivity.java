c3912b6c12fdf9e9eeaf97ac52fdb189a827775f
liuyao-s <liuyao-s@360.cn>
2017-05-03 11:44:06 +0800

    develop finish

--- app/src/main/java/com/example/liuyao_s/gittest/MainActivity.java
+++ app/src/main/java/com/example/liuyao_s/gittest/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("ly","develop commit 1");
+        Log.i("ly","develop work finish");
     }
 }
